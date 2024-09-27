95d2e281a43e8e3b8e72a173694d8fa9fcb59f7f
Kush <goyal.kush@hotmail.com>
2024-09-28 01:30:29 +0530

    Technologies section added

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,40 @@
+import { RiReactjsLine } from "react-icons/ri";
+import { TbBrandNextjs } from "react-icons/tb";
+import { SiMongodb } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
+import { TbSql } from "react-icons/tb";
+import { FaJava } from "react-icons/fa6";
+import { SiJavascript } from "react-icons/si";
+
+const Technologies = () => {
+  return (
+    <div className="border-b border-neutral-800 pb-24">
+      <h2 className="my-20 text-center text-4xl">Technologies</h2>
+      <div className="flex flex-wrap items-center justify-center gap-4">
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <RiReactjsLine className="text-7xl text-cyan-400" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <TbBrandNextjs className="text-7xl" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiMongodb className="text-7xl text-green-500" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiJavascript className="text-7xl text-yellow-500" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <TbSql className="text-7xl text-cyan-400" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaNodeJs className="text-7xl text-green-500" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaJava className="text-7xl text-cyan-400" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Technologies;
